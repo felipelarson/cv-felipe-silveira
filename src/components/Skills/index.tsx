58f3c32d67eafab9362faced05990b60f901774c
Felipe Larson da Silveira <felipe.silveira@enline-transmission.com>
2022-12-19 23:16:39 +0000

    chore: upgraded skills

--- src/components/Skills/index.tsx
+++ src/components/Skills/index.tsx
@@ -2,40 +2,27 @@ const techs_list = [
   "HTML",
   "CSS",
   "JavaScript",
-  "PHP",
-  "Python",
   "TypeScript",
   "React",
-  "Redux",
+  "NextJS",
   "ContextAPI",
   "Git",
-  // "GitFlow",
-  "CodeIgniter",
-  "Flask",
   "NodeJS",
   "Express",
-  "Django",
-  "MySql",
+  "Strapi",
   "Postegresql",
   "MongoDB",
-  "Jest",
-  "API Rest",
   "Inglês - B1",
 ];
-
 
 export const Skills = () => (
   <article>
     <h2>qualificações</h2>
 
     <ul className="skills">
-      {
-        techs_list.map((elm, idx) => {
-          return (
-            <li key={idx}>{elm}</li>
-          )
-        })
-      }
+      {techs_list.map((elm, idx) => {
+        return <li key={idx}>{elm}</li>;
+      })}
     </ul>
   </article>
-)
+);
